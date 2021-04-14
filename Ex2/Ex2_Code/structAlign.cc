29f9fe0de9c32b8bbff4cebd74b092dd09c9ce6c
rina.karnauch <rina.karnauch@mail.huji.ac.il>
2021-04-14 13:50:32 +0300

    added ex2 files

---
+++ Ex2/Ex2_Code/structAlign.cc
@@ -0,0 +1,16 @@
+//
+// Created by Rina Karnauch & Ofek Kaveh on 14/04/2021.
+//
+
+
+/**
+ * main for the struct alignment
+ * @param argc amount of args to the program
+ * @param argv array of arguments as chars
+ * @return none
+ */
+int main(int argc , char* argv[])
+
+}
+
+
