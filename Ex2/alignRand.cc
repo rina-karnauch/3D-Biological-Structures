dd3ddb06f895a35d147ec785653870025dead5b1
rina.karnauch <rina.karnauch@mail.huji.ac.il>
2021-04-24 11:59:42 +0300

    fixed all

--- Ex2/alignRand.cc
+++ Ex2/alignRand.cc
@@ -123,3 +123,265 @@
 //  std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
 //
 //}
+
+//
+// Created by Rina Karnauch & Ofek Kaveh on 14/04/2021.
+//
+
+//
+///**
+// * main for the struct alignment
+// * @param argc amount of args to the program
+// * @param argv array of arguments as chars
+// * @return none
+// */
+//#include "Vector3.h"
+//#include "Atom.h"
+//#include "RigidTrans3.h"
+//#include "Matrix3.h"
+//#include "Molecule.h"
+//#include "PDB.h"
+//#include "Match.h"
+//#include "GeomHash.h"
+//#include <chrono>
+//#include <iostream>
+//#include "Triangle.h"
+//
+//using namespace std;
+//
+//
+//vector<Triangle> makeCombinations(const Molecule<Atom> &molecule)
+//{
+//	vector<Triangle> triangles;
+//	for (int i = 0; i < molecule.size() - 2; i++)
+//	{
+//
+//		Atom x = molecule[i];
+//		Atom y = molecule[i + 1];
+//		Atom z = molecule[i + 2];
+//		Triangle t = Triangle(x, y, z);
+//		triangles.push_back(t);
+//	}
+//	return triangles;
+//}
+//
+//RigidTrans3 getTransformation(const Triangle &model, const Triangle &target)
+//{
+//	return target | model;
+//}
+//
+//vector<RigidTrans3>
+//getAllTransformations(const vector<Triangle> &mulModel,
+//					  const vector<Triangle> &mulTarget)
+//{
+//
+//	vector<RigidTrans3> transformations;
+//	for (auto &tr : mulTarget)
+//	{
+//		for (auto &mo : mulModel)
+//		{
+//			RigidTrans3 current = getTransformation(mo, tr);
+//
+//			transformations.push_back(current);
+//		}
+//	}
+//	return transformations;
+//
+//}
+//
+//int main(int argc, char *argv[])
+//{
+//	// measure the run time
+//	auto start = std::chrono::system_clock::now();
+//
+//	if (argc != 4)
+//	{
+//		std::cerr << "Usage: " << argv[0] << " dist_threshold pdb1 pdb2"
+//				  << std::endl;
+//		exit(1);
+//	}
+//
+//	// ********************Parameters********************
+//	float m_fDistThr = atof(argv[3]); // distance threshold on atoms in correspondence
+//
+//	std::cout << "Distance threshold: " << m_fDistThr << std::endl;
+//
+//	// read the two files into Molecule
+//	Molecule<Atom> molModel, molTarget, molModelFull;
+//
+//	std::ifstream fileModel(argv[2]);
+//	std::ifstream fileModelFull(argv[2]);
+//	std::ifstream fileTarget(argv[3]);
+//
+//	if (!fileModel)
+//	{
+//		std::cout << "File " << argv[1] << "does not exist." << std::endl;
+//		return 0;
+//	}
+//	if (!fileTarget)
+//	{
+//		std::cout << "File " << argv[2] << "does not exist." << std::endl;
+//		return 0;
+//	}
+//
+//	// reading the model with CA's and also reading the full model itself.
+//	int modelProteinSize = molModel.readPDBfile(fileModel, PDB::CAlphaSelector());
+//	int modelTargetSize = molTarget.readPDBfile(fileTarget, PDB::CAlphaSelector());
+//	molModelFull.readPDBfile(fileModelFull, PDB::AllSelector());
+//
+//	// one is RNA and second is Protein or otherwise
+//	if (((modelProteinSize == 0) && (modelTargetSize > 0)) ||
+//		((modelProteinSize) > 0 && (modelTargetSize == 0)))
+//	{
+//		std::cout << "Alignment Not Possible between Protein and RNA" << std::endl;
+//		return 0;
+//	}
+//		// if no CA's found then its a RNA mol
+//	else if ((modelProteinSize == 0) && (modelTargetSize == 0))
+//	{
+//		modelProteinSize = molModel.readPDBfile(fileModel, PDB::PSelector());
+//		modelTargetSize = molTarget.readPDBfile(fileTarget, PDB::PSelector());
+//	}
+//
+//	if ((modelProteinSize == 0) && (modelTargetSize == 0))
+//	{
+//		std::cout << "No Molecules Found." << std::endl;
+//		return 0;
+//	}
+//
+//	// calculate center of mass
+//	Vector3 vectModelMass(0, 0, 0);
+//	Vector3 vectModelMassFull(0, 0, 0);
+//	for (unsigned int i = 0; i < molModel.size(); i++)
+//	{
+//		vectModelMass += molModel[i].position();
+//	}
+//	vectModelMass /= molModel.size();
+//
+//	Vector3 vectTargetMass(0, 0, 0);
+//	Vector3 vectTargetMassFull(0, 0, 0);
+//	for (unsigned int i = 0; i < molTarget.size(); i++)
+//	{
+//		vectTargetMass += molTarget[i].position();
+//	}
+//	vectTargetMass /= molTarget.size();
+//
+//	// transform the molecules to the center of the coordinate system
+//	molModel += (-vectModelMass);
+//	molTarget += (-vectTargetMass);
+//
+//	// next we insert the target molecule into hash
+//	// this will help us to find atoms that are close faster
+//	GeomHash<Vector3, int> gHash(3,
+//								 m_fDistThr); // 3 is a dimension and m_fDistThr is the size of the hash cube
+//	for (unsigned int i = 0; i < molTarget.size(); i++)
+//	{
+//		gHash.insert(molTarget[i].position(),
+//					 i); // coordinate is the key to the hash, we store atom index
+//	}
+//
+//	// now molecules are both from the same genre
+//	vector<Triangle> modelTriangles = makeCombinations(molModel);
+//	vector<Triangle> targetTriangles = makeCombinations(molTarget);
+//
+//	for (int i = 0; i < molModel.size() - 3; i++)
+//	{
+//		Triangle mol = Triangle(molModel[i], molModel[i+1], molModel[i+2]);
+//		for (int j = 0; j < molTarget.size() - 3; j++)
+//		{
+//			Triangle tr = Triangle(molTarget[i], molTarget[i+3], molTarget[i+2]);
+//			RigidTrans3 currentTransformation = mol | tr;
+//			Match match = Match(currentTransformation);
+//
+//			for (unsigned int k = 0; k < molModel.size(); k++)
+//			{
+//
+//				Vector3 mol_atom = currentTransformation.rotation() * molModel[k].position(); //
+//				// rotate
+//
+//				// find close target molecule atoms using the hash
+//				HashResult<int> result;
+//				gHash.query(mol_atom, m_fDistThr, result); // key is mol atom coordinate
+//
+//				// check if the atoms in the result are inside the distance threshold
+//				// the hash is a cube shape, there can be atoms further that the threshold
+//				for (auto x = result.begin(); x != result.end(); x++)
+//				{
+//					float dist = mol_atom.dist(molTarget[*x].position());
+//					if (dist <= m_fDistThr)
+//					{
+//						float score = (1 / (1 + dist));
+//						match.add(*x, k, score, score);
+//					}
+//				}
+//				result.clear();
+//			}
+//		}
+//	}
+//
+//	vector<RigidTrans3> transformations = getAllTransformations(modelTriangles, targetTriangles);
+//
+//	// now we try rotations and choose the best alignment from random rotations
+//	unsigned int iMaxSize = 0;
+//	RigidTrans3 rtransBest;
+//	float RMSD = 0.0;
+//
+//	for (auto trans : transformations)
+//	{
+//
+//		// match is a class that stores the correspondence list, eg.
+//		// pairs of atoms, one from each molecule, that are matching
+//		Match match = Match(trans);
+//		Matrix3 transformation_matrix = trans.rotation();
+//
+//		// apply rotation on each atom in the model molecule and
+//		// add the pairs of atoms (one from target and one from model)
+//		// that are close enough to the match list
+//		for (unsigned int i = 0; i < molModel.size(); i++)
+//		{
+//
+//			Vector3 mol_atom = transformation_matrix * molModel[i].position(); // rotate
+//
+//			// find close target molecule atoms using the hash
+//			HashResult<int> result;
+//			gHash.query(mol_atom, m_fDistThr, result); // key is mol atom coordinate
+//
+//			// check if the atoms in the result are inside the distance threshold
+//			// the hash is a cube shape, there can be atoms further that the threshold
+//			for (auto x = result.begin(); x != result.end(); x++)
+//			{
+//				float dist = mol_atom.dist(molTarget[*x].position());
+//				if (dist <= m_fDistThr)
+//				{
+//					float score = (1 / (1 + dist));
+//					match.add(*x, i, score, score);
+//				}
+//			}
+//			result.clear();
+//		}
+//
+//		match.calculateBestFit(molTarget, molModel);
+//
+//		if (iMaxSize < match.size())
+//		{
+//			iMaxSize = match.size();
+//			rtransBest = match.rigidTrans();
+//			RMSD = match.rmsd();
+//		}
+//	}
+//
+//	std::cout << "Max Alignment Size: " << iMaxSize << std::endl;
+//	std::cout << "Achieved RMSD is:" << RMSD << std::endl;
+//	std::cout << "Rigid Trans: " << rtransBest << std::endl;
+//
+//	auto end = std::chrono::system_clock::now();
+//
+//	std::chrono::duration<double> elapsed_seconds = end - start;
+//	std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
+//
+//
+//	molModelFull *= rtransBest;
+//
+//	std::ofstream ofstream("transformed.pdb", std::ofstream::out);
+//	ofstream << molModelFull;
+//}
--- Ex2/structAlign.cc
+++ Ex2/structAlign.cc
@@ -12,12 +12,10 @@
 #include "Vector3.h"
 #include "Atom.h"
 #include "RigidTrans3.h"
-#include "Matrix3.h"
 #include "Molecule.h"
 #include "PDB.h"
 #include "Match.h"
 #include "GeomHash.h"
-#include <chrono>
 #include <iostream>
 #include "Triangle.h"
 
@@ -25,9 +23,14 @@ using namespace std;
 
 
 vector<Triangle> makeCombinations(const Molecule<Atom> &molecule)
+/**
+ * method to get all triangle combinations out of atom
+ * @param molecule: current molecule to turn into triangles
+ * @return vector of all triangles of conseccutive CA triangles
+ */
 {
 	vector<Triangle> triangles;
-	for (int i = 0; i < molecule.size() - 3; i++)
+	for (int i = 0; i < molecule.size() - 2; i++)
 	{
 
 		Atom x = molecule[i];
@@ -39,7 +42,13 @@ vector<Triangle> makeCombinations(const Molecule<Atom> &molecule)
 	return triangles;
 }
 
-RigidTrans3 getTransformation(Triangle model, Triangle target)
+RigidTrans3 getTransformation(const Triangle& model, const Triangle& target)
+/**
+ * method to get transformation between two triangles
+ * @param model model triangle to put onto
+ * @param target target triangle to put model ontop of it
+ * @return a rigidtrans of model->target
+ */
 {
 	return target | model;
 }
@@ -47,6 +56,12 @@ RigidTrans3 getTransformation(Triangle model, Triangle target)
 vector<RigidTrans3>
 getAllTransformations(vector<Triangle> mulModel,
 					  vector<Triangle> mulTarget)
+/**
+ * method to get all possible transformations out of all triangles
+ * @param mulModel vector of all model triangles
+ * @param mulTarget vector of all target triangles
+ * @return vector of all transformation
+ */
 {
 	vector<RigidTrans3> transformations;
 	for (int i = 0; i < mulModel.size(); i++)
@@ -60,170 +75,159 @@ getAllTransformations(vector<Triangle> mulModel,
 
 }
 
+
+
 int main(int argc, char *argv[])
+/**
+ * main to apply transformations and find best
+ * @return 0 for success 1 for failure
+ */
 {
-	// measure the run time
-	auto start = std::chrono::system_clock::now();
+    // measure the run time
+//    auto start = std::chrono::system_clock::now();
 
-	if (argc != 4)
-	{
-		std::cerr << "Usage: " << argv[0] << " target.pdb model.pdb dist_threshold"
-				  << std::endl;
-		exit(1);
-	}
+    if (argc != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " dist_threshold pdb1 pdb2"
+                  << std::endl;
+        exit(1);
+    }
 
-	// ********************Parameters********************
-	float m_fDistThr = atof(argv[3]); // distance threshold on atoms in correspondence
+    // ********************Parameters********************
+    float m_fDistThr = atof(argv[1]); // distance threshold on atoms in correspondence
 
-	std::cout << "Distance threshold: " << m_fDistThr << std::endl;
+//    std::cout << "Distance threshold: " << m_fDistThr << std::endl;
 
-	// read the two files into Molecule
-	Molecule<Atom> molModel, molTarget, molModelFull;
+    // read the two files into Molecule
+    Molecule<Atom> molModel, molTarget, molModelFull;
 
-	std::ifstream fileModel(argv[2]);
-	std::ifstream fileModelFull(argv[2]);
-	std::ifstream fileTarget(argv[1]);
+    std::ifstream fileModel(argv[3]);
+    std::ifstream fileTarget(argv[2]);
+    std::ifstream savedModel(argv[3]);
+    std::ifstream tempRead(argv[3]);
 
-	if (!fileModel)
-	{
-		std::cout << "File " << argv[1] << "does not exist." << std::endl;
-		return 0;
-	}
-	if (!fileTarget)
-	{
-		std::cout << "File " << argv[2] << "does not exist." << std::endl;
-		return 0;
-	}
+    if (!fileModel)
+    {
+        std::cout << "File " << argv[3] << "does not exist." << std::endl;
+        return 0;
+    }
+    if (!fileTarget)
+    {
+        std::cout << "File " << argv[2] << "does not exist." << std::endl;
+        return 0;
+    }
 
-//
-	int modelProteinSize = molModel.readPDBfile(fileModel, PDB::CAlphaSelector());
-	int modelTargetSize = molTarget.readPDBfile(fileTarget, PDB::CAlphaSelector());
-	molModelFull.readPDBfile(fileModelFull, PDB::AllSelector());
+    std::string lineRead;
+    std::getline(tempRead, lineRead);
+    savedModel.clear();
+    molModelFull.readPDBfile(savedModel, PDB::AllSelector());
+
+	// if first line is RNA
+    if (lineRead.find("RNA") != std::string::npos)
+    {
+        molModel.readPDBfile(fileModel, PDB::PSelector());
+        molTarget.readPDBfile(fileTarget, PDB::PSelector());
+    }
+    else
+    {
+    	// otherwise we read it as a protein
+        molModel.readPDBfile(fileModel, PDB::CAlphaSelector());
+        molTarget.readPDBfile(fileTarget, PDB::CAlphaSelector());
+    }
 
 
-//	int modelProteinSize = molModel.readPDBfile(fileModel, PDB::AllSelector());
-//	int modelTargetSize = molTarget.readPDBfile(fileTarget, PDB::AllSelector());
+    // calculate center of mass
+    Vector3 vectModelMass(0, 0, 0);
+    for (unsigned int i = 0; i < molModel.size(); i++)
+    {
+        vectModelMass += molModel[i].position();
+    }
+    vectModelMass /= molModel.size();
 
-	// one is RNA and second is Protein or otherwise
-	if (((modelProteinSize == 0) && (modelTargetSize > 0)) ||
-		((modelProteinSize) > 0 && (modelTargetSize == 0)))
-	{
-		std::cout << "Alignment Not Possible between Protein and RNA" << std::endl;
-		return 0;
-	}
-		// if no CA's found then its a RNA mol
-	else if ((modelProteinSize == 0) && (modelTargetSize == 0))
-	{
-		modelProteinSize = molModel.readPDBfile(fileModel, PDB::PSelector());
-		modelTargetSize = molTarget.readPDBfile(fileTarget, PDB::PSelector());
-	}
+    Vector3 vectTargetMass(0, 0, 0);
+    for (unsigned int i = 0; i < molTarget.size(); i++)
+    {
+        vectTargetMass += molTarget[i].position();
+    }
+    vectTargetMass /= molTarget.size();
 
-	if ((modelProteinSize == 0) && (modelTargetSize == 0))
-	{
-		std::cout << "No Molecules Found." << std::endl;
-		return 0;
-	}
-
-	// calculate center of mass
-	Vector3 vectModelMass(0, 0, 0);
-	Vector3 vectModelMassFull(0, 0, 0);
-	for (unsigned int i = 0; i < molModel.size(); i++)
-	{
-		vectModelMass += molModel[i].position();
-	}
-	vectModelMass /= molModel.size();
-
-	Vector3 vectTargetMass(0, 0, 0);
-	Vector3 vectTargetMassFull(0, 0, 0);
-	for (unsigned int i = 0; i < molTarget.size(); i++)
-	{
-		vectTargetMass += molTarget[i].position();
-	}
-	vectTargetMass /= molTarget.size();
-
-	// transform the molecules to the center of the coordinate system
-	molModel += (-vectModelMass);
-	molTarget += (-vectTargetMass);
+    // transform the molecules to the center of the coordinate system
+    molModel += (-vectModelMass);
+    molTarget += (-vectTargetMass);
 
 	// next we insert the target molecule into hash
 	// this will help us to find atoms that are close faster
-	GeomHash<Vector3, int> gHash(3,
-								 m_fDistThr); // 3 is a dimension and m_fDistThr is the size of the hash cube
+	GeomHash<Vector3, int> gHash(3, m_fDistThr); // 3 is a dimension and m_fDistThr is the size of the hash cube
 	for (unsigned int i = 0; i < molTarget.size(); i++)
 	{
-		gHash.insert(molTarget[i].position(),
-					 i); // coordinate is the key to the hash, we store atom index
+		gHash.insert(molTarget[i].position(), i); // coordinate is the key to the hash, we store atom index
 	}
 
-	// now molecules are both from the same genre
-	vector<Triangle> modelTriangles = makeCombinations(molModel);
-	vector<Triangle> targetTriangles = makeCombinations(molTarget);
+    // now we try rotations and choose the best alignment from random rotations
+    unsigned int iMaxSize = 0;
+    RigidTrans3 rtransBest;
+    float RMSD;
 
-	vector<RigidTrans3> transformations = getAllTransformations(modelTriangles, targetTriangles);
+    vector<Triangle> modelTriangles = makeCombinations(molModel);
+    vector<Triangle> targetTriangles = makeCombinations(molTarget);
 
-	// now we try rotations and choose the best alignment from random rotations
-	unsigned int iMaxSize = 0;
-	RigidTrans3 rtransBest = transformations[0];
-	float RMSD = 0.0;
+    vector<RigidTrans3> transformations = getAllTransformations(modelTriangles, targetTriangles);
 
-	for (int it = 0; it < transformations.size(); it++)
-	{
+    for (int k = 0; k < transformations.size(); k++)
+    {
+        Match match;
+        // apply rotation on each atom in the model molecule and
+        // add the pairs of atoms (one from target and one from model)
+        // that are close enough to the match list
+        for (unsigned int i = 0; i < molModel.size(); i++)
+        {
+            Vector3 mol_atom = transformations[k] * molModel[i].position(); // rotate
 
-		// match is a class that stores the correspondence list, eg.
-		// pairs of atoms, one from each molecule, that are matching
-		RigidTrans3 trans = transformations[it];
-		Match match = Match(trans);
-		Matrix3 transformation_matrix = trans.rotation();
+            // find close target molecule atoms using the hash
+            HashResult<int> result;
+            gHash.query(mol_atom, m_fDistThr, result); // key is mol atom coordinate
 
-		// apply rotation on each atom in the model molecule and
-		// add the pairs of atoms (one from target and one from model)
-		// that are close enough to the match list
-		for (unsigned int i = 0; i < molModel.size(); i++)
-		{
-			Vector3 mol_atom = transformation_matrix * molModel[i].position(); // rotate
+            // check if the atoms in the result are inside the distance threshold
+            // the hash is a cube shape, there can be atoms further that the threshold
+            for (auto x = result.begin(); x != result.end(); x++)
+            {
+                float dist = mol_atom.dist(molTarget[*x].position());
+                if (dist <= m_fDistThr)
+                {
+                    float score = (1 / (1 + dist));
+                    match.add(*x, i, score, score);
+                }
+            }
+            result.clear();
+        }
 
-			// find close target molecule atoms using the hash
-			HashResult<int> result;
-			gHash.query(mol_atom, m_fDistThr, result); // key is mol atom coordinate
+        match.calculateBestFit(molTarget, molModel);
 
-			// check if the atoms in the result are inside the distance threshold
-			// the hash is a cube shape, there can be atoms further that the threshold
-			for (auto x = result.begin(); x != result.end(); x++)
-			{
-				float dist = mol_atom.dist(molTarget[*x].position());
-				if (dist <= m_fDistThr)
-				{
-					float score = (1 / (1 + dist));
-					match.add(*x, i, score, score);
-				}
-			}
-			result.clear();
-		}
+        if (iMaxSize < match.size())
+        {
+            iMaxSize = match.size();
+            rtransBest = match.rigidTrans();
+            RMSD = match.rmsd();
+        }
+    }
 
-		match.calculateBestFit(molTarget, molModel);
+    std::cout << iMaxSize << " " << RMSD << " " << rtransBest << " " << std::endl;
 
-		if (iMaxSize < match.size())
-		{
-			iMaxSize = match.size();
-			rtransBest = match.rigidTrans();
-			RMSD = match.rmsd();
-		}
-	}
+//	std::cout << "Max Alignment Size: " << iMaxSize << std::endl;
+//	std::cout << "Achieved RMSD is:" << RMSD << std::endl;
+//	std::cout << "Rigid Trans: " << rtransBest << std::endl;
 
-	std::cout << "Max Alignment Size: " << iMaxSize << std::endl;
-	std::cout << "Achieved RMSD is:" << RMSD << std::endl;
-	std::cout << "Rigid Trans: " << rtransBest << std::endl;
+//	auto end = std::chrono::system_clock::now();
 
-	auto end = std::chrono::system_clock::now();
-
-	std::chrono::duration<double> elapsed_seconds = end - start;
-	std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
+//	std::chrono::duration<double> elapsed_seconds = end - start;
+//	std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
 
 
 	molModelFull *=rtransBest;
 
-	std::ofstream ofstream("transformed.pdb", std::ofstream::out);
-	ofstream << molModelFull;
+	std::ofstream fileStream("transformed.pdb", std::ofstream::out);
+	fileStream << molModelFull;
 }
+
 
 
