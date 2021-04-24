3e110030769bb2b9fe175c9173e13aac740c760e
rina.karnauch <rina.karnauch@mail.huji.ac.il>
2021-04-24 13:12:28 +0300

    finished and made tar

---
+++ Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/Atom.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/Atom.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/Match.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/Match.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/Matrix3.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/Matrix3.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/PDB.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/PDB.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/RigidTrans3.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/RigidTrans3.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/Rotation3.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/Rotation3.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/Triangle.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/Triangle.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/Vector3.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/Vector3.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/alignRand.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/alignRand.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/numerics.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/numerics.cc.o"
+  "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/structAlign.cc" "/Users/rina.karnauch/Desktop/pdfs/3D/Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/structAlign.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Ex2/cmake-build-debug/CMakeFiles/Ex2.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex2.dir/Atom.cc.o"
+  "CMakeFiles/Ex2.dir/Match.cc.o"
+  "CMakeFiles/Ex2.dir/Matrix3.cc.o"
+  "CMakeFiles/Ex2.dir/PDB.cc.o"
+  "CMakeFiles/Ex2.dir/RigidTrans3.cc.o"
+  "CMakeFiles/Ex2.dir/Rotation3.cc.o"
+  "CMakeFiles/Ex2.dir/Triangle.cc.o"
+  "CMakeFiles/Ex2.dir/Vector3.cc.o"
+  "CMakeFiles/Ex2.dir/alignRand.cc.o"
+  "CMakeFiles/Ex2.dir/numerics.cc.o"
+  "CMakeFiles/Ex2.dir/structAlign.cc.o"
+  "Ex2"
+  "Ex2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
