75610f50800363414120210fbfc2c51fd9ce0239
rina.karnauch <rina.karnauch@mail.huji.ac.il>
2021-05-14 13:43:10 +0300

    q7+q8 done

--- Ex3/main.py
+++ Ex3/main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+plt.style.use('seaborn-whitegrid')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def question_5():
+    csv_data = np.genfromtxt('H3_modeling_100_scores.csv', delimiter=',',
+                             skip_header=1, usecols=range(1, 3))
+    # y is total score
+    y = [val[0] for val in csv_data]
+    # X is RMSD
+    X = [val[1] for val in csv_data]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    plt.scatter(X, y, marker='o')
+    plt.xlabel('RMSD')
+    plt.ylabel('rosetta scores')
+    plt.show()
+
+
+if __name__ == "__main__":
+    question_5()
